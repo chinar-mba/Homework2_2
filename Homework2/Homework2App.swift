f445a3c816074b309ad3a8bb6276372f319383ca
chinar-mba <chinar.mba@gmail.com>
2023-05-16 23:35:09 -0400

    Initial Commit

---
+++ Homework2/Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2App.swift
+//  Homework2
+//
+//  Created by Chinara on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
